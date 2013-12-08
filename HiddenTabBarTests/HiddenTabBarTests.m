b16c73a8d772baef22e460ff33fa3bdf50558121
Naoki Morita <namorit@gmail.com>
2013-12-08 15:31:21 +0900

    add contents...

---
+++ HiddenTabBarTests/HiddenTabBarTests.m
@@ -0,0 +1,34 @@
+//
+//  HiddenTabBarTests.m
+//  HiddenTabBarTests
+//
+//  Created by Morita Naoki on 2013/12/08.
+//  Copyright (c) 2013年 molabo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HiddenTabBarTests : XCTestCase
+
+@end
+
+@implementation HiddenTabBarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
